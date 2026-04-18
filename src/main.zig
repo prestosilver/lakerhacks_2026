e57914d8e337287da4a832706f51ac9f11ae47e9
bob16795 <prestosilver.ptp@gmail.com>
2026-04-18 07:43:26 -0500

    Cleanup shell and move some consts

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,14 @@
 const std = @import("std");
 const rl = @import("raylib");
 
+const SCREEN_WIDTH = 800;
+const SCREEN_HEIGHT = 450;
+
+// zig+emscripten only works with c malloc/free
+const allocator = std.heap.c_allocator;
+
 pub fn main() anyerror!void {
-    _ = std;
-
-    const screenWidth = 800;
-    const screenHeight = 450;
-
-    rl.initWindow(screenWidth, screenHeight, "Lakerhacks 2026");
+    rl.initWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Lakerhacks 2026");
     defer rl.closeWindow();
 
     rl.setTargetFPS(60);
