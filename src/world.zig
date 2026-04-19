513c237cac042ed3470389320e7e2e3d103fdfc6
bob16795 <prestosilver.ptp@gmail.com>
2026-04-19 07:50:07 -0500

    Start UI rework

--- src/world.zig
+++ src/world.zig
@@ -31,23 +31,13 @@ var factions_aux: std.ArrayList(Faction) = undefined;
 
 var links: std.ArrayList(Link) = undefined;
 
-pub const UserInput = struct
-{
-    mouse_world_pos: rl.Vector2,
-    lmb: bool,
-    rmb: bool
-};
+pub const UserInput = struct { mouse_world_pos: rl.Vector2, lmb: bool, rmb: bool };
 
-pub const UIMode = enum
-{
-    Game,
-    Linking
-};
+pub const UIMode = enum { Game, Linking };
 
 pub var ui_mode: UIMode = undefined;
 
-fn generate_world(camera: *Camera) void
-{
+fn generate_world(camera: *Camera) void {
     for (0..GRID_SIZE) |i| for (0..GRID_SIZE) |j| {
         // TODO: Stretch goal: make the galaxy spiral out. (might take an hour or two).
 
@@ -76,11 +66,9 @@ fn generate_world(camera: *Camera) void
 
     var num_factions = rl.getRandomValue(6, 25);
     var cur_faction: usize = 1;
-    while(num_factions > 0) : (num_factions -= 1)
-    {
+    while (num_factions > 0) : (num_factions -= 1) {
         var host_star = @as(usize, @intCast(rl.getRandomValue(0, @intCast(stars_aux.items.len - 1))));
-        while(stars_aux.items[host_star].owner != 0)
-        {
+        while (stars_aux.items[host_star].owner != 0) {
             // Make sure the star isn't already owned by another faction.
             host_star = @as(usize, @intCast(rl.getRandomValue(0, @intCast(stars_aux.items.len - 1))));
         }
@@ -90,27 +78,24 @@ fn generate_world(camera: *Camera) void
         const faction: Faction = .init(stars_aux.items[host_star]);
         factions_aux.appendAssumeCapacity(faction);
 
-        if(cur_faction == 1)
-        {
+        if (cur_faction == 1) {
             const star_pos = stars_aux.items[host_star].getStarWorldPos(true);
             camera.x = star_pos.x;
             camera.y = star_pos.y;
 
-            std.debug.print("{d}, {d}\n", .{camera.x, camera.y});
+            std.debug.print("{d}, {d}\n", .{ camera.x, camera.y });
         }
 
         cur_faction += 1;
     }
 
-    if (@import("builtin").mode == .Debug)
-    {
+    if (@import("builtin").mode == .Debug) {
         std.debug.print("Total stars: {d}.\n", .{stars_aux.items.len});
         std.debug.print("Total factions: {d}.\n", .{factions_aux.items.len});
     }
 }
 
-pub fn draw(camera: Camera) void
-{
+pub fn draw(camera: Camera) void {
     rl.clearBackground(.{ .r = 0, .g = 0, .b = 0, .a = 255 });
 
     for (links.items) |link| {
@@ -125,8 +110,7 @@ pub fn draw(camera: Camera) void
     }
 }
 
-pub fn init(camera: *Camera) void
-{
+pub fn init(camera: *Camera) void {
     links = .initBuffer(&link_buffer);
 
     stars_aux = .initBuffer(&stars_aux_buffer);
@@ -140,8 +124,7 @@ pub fn init(camera: *Camera) void
     generate_world(camera);
 }
 
-fn linkStars(index_a: usize, index_b: usize) void
-{
+fn linkStars(index_a: usize, index_b: usize) void {
     const link: Link = .init(stars_aux.items[index_a], stars_aux.items[index_b]);
     links.appendAssumeCapacity(link);
 
@@ -149,13 +132,18 @@ fn linkStars(index_a: usize, index_b: usize) void
     ui_mode = .Game;
 }
 
-fn selectStar(index: usize) void
-{
+fn selectStar(index: usize) void {
     star_selection = @intCast(index);
 }
 
-pub fn tick() void
-{
+pub fn selectedStar() ?*Star {
+    return if (star_selection >= 0)
+        stars_aux.items[@intCast(star_selection)]
+    else
+        null;
+}
+
+pub fn tick() void {
     for (stars_aux.items) |star|
         star.tick();
 
@@ -163,39 +151,31 @@ pub fn tick() void
         link.tick();
 }
 
-pub fn updateInput(input: UserInput) void
-{
-    if(input.rmb)
-    {
+pub fn updateInput(input: UserInput) void {
+    if (input.rmb) {
         star_selection = -1;
         focused_star = -1;
 
         ui_mode = .Game;
     }
 
-    for (stars_aux.items, 0..) |star, i|
-    {
+    for (stars_aux.items, 0..) |star, i| {
         star.mouse_hovering = false;
-        if (rl.checkCollisionPointRec(input.mouse_world_pos, star.getStarRectangle()))
-        {
+        if (rl.checkCollisionPointRec(input.mouse_world_pos, star.getStarRectangle())) {
             star.mouse_hovering = true;
-            if(input.lmb)
-            {
-                switch(ui_mode)
-                {
+            if (input.lmb) {
+                switch (ui_mode) {
                     .Game => selectStar(i),
-                    .Linking =>
-                    {
+                    .Linking => {
                         std.debug.assert(star_selection != -1);
                         linkStars(i, @intCast(star_selection));
-                    }
+                    },
                 }
             }
         }
     }
 
-    if(star_selection != -1 and rl.isKeyPressed(.kp_1))
-    {
+    if (star_selection != -1 and rl.isKeyPressed(.kp_1)) {
         ui_mode = .Linking;
     }
 }
